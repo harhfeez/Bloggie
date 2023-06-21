00dc2b12bd930aa65cb244addc1bd01bd8182717
harhfeez <fasasihafis1@gmail.com>
2023-06-21 15:12:13 +0100

    Bloggie App

--- blog/settings.py
+++ blog/settings.py
@@ -22,12 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY', ""))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = ['*']
+PROTOCOL = "http"
+DOMAIN = "localhost:8080"
+
+if not DEBUG:
+    PROTOCOL = "http"
+    DOMAIN = "bloggie-qsao.onrender.com"
+    ALLOWED_HOSTS = ["bloggie-qsao.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://bloggie-qsao.onrender.com"]
+
+
+ALLOWED_HOSTS = ["localhost",'127.0.0.1', "0.0.0.0",'bloggie-qsao.onrender.com']
 
 
 # Application definition
